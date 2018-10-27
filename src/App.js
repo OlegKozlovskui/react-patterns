4af2d479f3fa8487e93bfbf024be7a01eb050c64
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2018-10-27 15:16:26 +0300

    render props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        <Counter>
+          {({ count, increment, decrement }) =>
+            <div className="counter">
+              <button type="button" className="btn btn-primary" onClick={increment}>+</button>
+              <h3>{count}</h3>
+              <button type="button" className="btn btn-warning" onClick={decrement}>-</button>
+            </div>
+          }
+        </Counter>
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -5,21 +5,17 @@ class Counter extends Component {
     count: 0
   };
 
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  handleIncrement = () => this.setState(state => ({ count: state.count + 1 }));
 
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+  handleDecrement = () => this.setState(state => ({ count: state.count - 1 }));
 
   render() {
     return (
-      <div className="counter">
-        <button type="button" className="btn btn-primary" onClick={this.increment}>+</button>
-        <h3>{this.state.count}</h3>
-        <button type="button" className="btn btn-warning" onClick={this.decrement}>-</button>
-      </div>
+      this.props.children({
+        count: this.state.count,
+        increment: this.handleIncrement,
+        decrement: this.handleDecrement,
+      })
     );
   }
 }
